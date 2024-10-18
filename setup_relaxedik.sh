443d335b09594283355fd04ff3fa112d643326e4
Aren <arenkdesai@gmail.com>
2024-10-17 21:43:07 -0500

    Adding relaxedik_ros2 setup

---
+++ setup_relaxedik.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /workspace/src/relaxed_ik_ros2
+git config --global --add safe.directory /workspace
+git submodule init
+git submodule update
+cd relaxed_ik_core
+cargo build
+cd /workspace/src
+colcon build --symlink-install
+. install/setup.bash
