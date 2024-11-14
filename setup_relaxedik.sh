d6fdc8077e3cc4e987bef014bad0a596dadcb9ce
Aren <arenkdesai@gmail.com>
2024-11-13 18:22:59 -0600

    Deleting setup_relaxedik (deprecated)

--- setup_relaxedik.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /workspace/src/relaxed_ik_ros2
-git config --global --add safe.directory /workspace
-git submodule init
-git submodule update
-cd relaxed_ik_core
-cargo build
-cd /workspace/src
-colcon build --symlink-install
