e1a6f2686ed446f073527d4f668960ce73bedd8b
Nicolas Dittmar Greaves <nicolasdittmarg1@gmail.com>
2025-01-21 18:03:09 -0600

    Created templete for robot, individual files can be changed

---
+++ build/sensors/CMakeFiles/gps_reading.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gps_reading.dir/src/gps_reading.cpp.o"
+  "CMakeFiles/gps_reading.dir/src/gps_reading.cpp.o.d"
+  "gps_reading"
+  "gps_reading.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gps_reading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
