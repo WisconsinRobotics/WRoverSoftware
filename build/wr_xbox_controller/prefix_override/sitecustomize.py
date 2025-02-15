933c2fefc2090ec6ab7ae1eb906275ab5d7654d2
ctx-devansh <devansh.the.photofreak@gmail.com>
2025-02-14 18:06:19 -0600

    Weird commit

--- build/wr_xbox_controller/prefix_override/sitecustomize.py
+++ build/wr_xbox_controller/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/wiscrobo/WRoverXboxController/WRoverSoftware/install/wr_xbox_controller'
+    sys.prefix = sys.exec_prefix = '/home/wiscrobo/WRoverSoftware/install/wr_xbox_controller'
