c8d20188eda6129f4ba4577408995f5aaffa706c
ctx-devansh <devansh.the.photofreak@gmail.com>
2025-02-16 19:08:07 -0600

    CODE not work

--- src/arm_test/src/arm_test_node.cpp
+++ src/arm_test/src/arm_test_node.cpp
@@ -1,42 +1,38 @@
+#define Phoenix_No_WPI // remove WPI dependencies
+#include "ctre/Phoenix.h"
+#include "ctre/phoenix/platform/Platform.hpp"
+#include "ctre/phoenix/unmanaged/Unmanaged.h"
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <unistd.h>
+#include <SDL2/SDL.h>
+
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32_multi_array.hpp"
-#include "ctre/phoenix6/TalonFX.hpp"
 
 using std::placeholders::_1;
+using namespace ctre::phoenix;
+using namespace ctre::phoenix::platform;
+using namespace ctre::phoenix::motorcontrol;
+using namespace ctre::phoenix::motorcontrol::can;
 
 class MinimalSubscriber : public rclcpp::Node
 {
 public:
     MinimalSubscriber()
-        : Node("arm_test")
+        : Node("arm_test"),
+          shoulderMotor(0, "can0"), // Initialize TalonSRX with ID 0 on CAN bus "can0"
+          elbowMotor(1, "can0") // Assume elbowMotor has a different ID (1)
     {
         subscription_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
             "arm", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-
-        static constexpr char const *CANBUS_NAME = "*";
-
-        /* Only one left and one right motor */
-        shoulderMotor = std::make_unique<ctre::phoenix6::hardware::TalonFX>(0, CANBUS_NAME);
-        elbowMotor = std::make_unique<ctre::phoenix6::hardware::TalonFX>(1, CANBUS_NAME);
-
-        /* control requests */
-        shoulderOut = std::make_unique<ctre::phoenix6::controls::DutyCycleOut>(0);
-        elbowOut = std::make_unique<ctre::phoenix6::controls::DutyCycleOut>(0);
-
-        ctre::phoenix6::configs::TalonFXConfiguration fx_cfg{};
-
-        /* Shoulder motor is CCW+ */
-        fx_cfg.MotorOutput.Inverted = ctre::phoenix6::signals::InvertedValue::CounterClockwise_Positive;
-        shoulderMotor->GetConfigurator().Apply(fx_cfg);
-
-        /* Elbow motor is CW+ */
-        fx_cfg.MotorOutput.Inverted = ctre::phoenix6::signals::InvertedValue::Clockwise_Positive;
-        elbowMotor->GetConfigurator().Apply(fx_cfg);
     }
 
 private:
-    void topic_callback(const std_msgs::msg::Float32MultiArray &msg) const
+    void topic_callback(const std_msgs::msg::Float32MultiArray &msg)
     {
         if (msg.data.size() < 3)
         {
@@ -47,18 +43,15 @@ private:
         double shoulder_speed = msg.data[1]; // SDL_CONTROLLER_AXIS_LEFTY
         double elbow_speed = msg.data[2];    // SDL_CONTROLLER_AXIS_LEFTY
 
-        shoulderOut->Output = shoulder_speed;
-        elbowOut->Output = elbow_speed;
-
-        shoulderMotor->SetControl(*shoulderOut);
-        elbowMotor->SetControl(*elbowOut);
+        shoulderMotor.Set(ControlMode::PercentOutput, shoulder_speed);
+        elbowMotor.Set(ControlMode::PercentOutput, elbow_speed);
     }
 
     rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscription_;
-    std::unique_ptr<ctre::phoenix6::hardware::TalonFX> shoulderMotor;
-    std::unique_ptr<ctre::phoenix6::hardware::TalonFX> elbowMotor;
-    std::unique_ptr<ctre::phoenix6::controls::DutyCycleOut> shoulderOut;
-    std::unique_ptr<ctre::phoenix6::controls::DutyCycleOut> elbowOut;
+
+    // Declare TalonSRX objects as class members
+    TalonSRX shoulderMotor;
+    TalonSRX elbowMotor;
 };
 
 int main(int argc, char *argv[])
@@ -68,4 +61,3 @@ int main(int argc, char *argv[])
     rclcpp::shutdown();
     return 0;
 }
-
