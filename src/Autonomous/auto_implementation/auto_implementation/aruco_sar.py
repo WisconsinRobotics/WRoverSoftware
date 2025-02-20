968f2d4c3fcbcf504f8809c1ce4881581e760380
Rus Milovanovic <cmilovanovic@wisc.edu>
2025-02-19 19:59:02 -0600

    Added working aruco detection without ROS. Will fix the version with ROS after SAR video.

--- src/Autonomous/auto_implementation/auto_implementation/aruco_sar.py
+++ src/Autonomous/auto_implementation/auto_implementation/aruco_sar.py
@@ -10,8 +10,8 @@ if not cap.isOpened():
     exit()
 
 # Create an ArUco dictionary and parameters for detection
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)  # You can choose a different dictionary
-parameters = aruco.DetectorParameters_create()
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters()
 
 while True:
     # Capture frame-by-frame
