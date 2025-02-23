bf7521580e11577d9fff50f45e994950d9daf6cf
Aren <arenkdesai@gmail.com>
2025-02-23 17:27:50 -0600

    Cleaning some unneeded files, adding template for encoder publisher

--- driverStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source install/local_setup.bash
-
-ros2 run wr_swerve_control swerve_control &&
-	ros2 run wr_swerve_motor swerve_motor
-
--- roverStart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source install/local_setup.bash
-
-ros2 run wr_can_comms can_comms
