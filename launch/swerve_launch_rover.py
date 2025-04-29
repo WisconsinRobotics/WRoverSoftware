aa0fd1c75be8747d0881cd58ac05eedeba2c5adb
Balabalu-Ve <60904972+Balabalu-VE@users.noreply.github.com>
2025-04-28 19:42:02 -0500

    Combined Code

--- launch/swerve_launch_rover.py
+++ launch/swerve_launch_rover.py
@@ -4,6 +4,21 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
+            package='arm_test',
+            executable='ik_test_node',
+            name='ik_test_node'
+        ),
+        Node(
+            package='arm_test_python',
+            executable='arm_test_neo',
+            name='arm_test_neo'
+        ),
+        Node(
+            package='arm_test_python',
+            executable='ik_subscriber',
+            name='ik_subscriber'
+        ),
+        Node(
             package='wr_can_comms',
             executable='can_comms',
             name='can_comms'
