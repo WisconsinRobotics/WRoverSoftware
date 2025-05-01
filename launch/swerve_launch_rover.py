c7a616e4579117d9f06ccb7cd7a4942e4e46c058
Nicolas Dittmar Greaves <nicolasdittmarg1@gmail.com>
2025-05-01 09:29:23 -0500

    Changes for demo

--- launch/swerve_launch_rover.py
+++ launch/swerve_launch_rover.py
@@ -3,20 +3,25 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        #Node(
+        #    package='arm_test',
+        #    executable='ik_test_node',
+        #    name='ik_test_node'
+        #),
+        #Node(
+        #    package='arm_test_python',
+        #    executable='arm_test_neo',
+        #    name='arm_test_neo'
+        #),
+        #Node(
+        #    package='arm_test_python',
+        #    executable='ik_subscriber',
+        #    name='ik_subscriber'
+        #),
         Node(
             package='arm_test',
-            executable='ik_test_node',
-            name='ik_test_node'
-        ),
-        Node(
-            package='arm_test_python',
-            executable='arm_test_neo',
-            name='arm_test_neo'
-        ),
-        Node(
-            package='arm_test_python',
-            executable='ik_subscriber',
-            name='ik_subscriber'
+            executable='arm_test_node',
+            name='arm_test'
         ),
         Node(
             package='wr_can_comms',
