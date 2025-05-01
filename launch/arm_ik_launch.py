d53d66325c2c4ee6a0849e636d7b5e21e6192b75
Nicolas Dittmar Greaves <nicolasdittmarg1@gmail.com>
2025-05-01 15:54:05 -0500

    Arm Better

--- launch/arm_ik_launch.py
+++ launch/arm_ik_launch.py
@@ -15,17 +15,25 @@ def generate_launch_description():
         ),
 
         #RUN GRIPPER WITH IK
-        # Node(
-        #     package='arm_test_python',
-        #     executable='ik_subscriber',
-        #     name='ik_subscriber'
-        # ),
-
-        #RUN GRIPPER W/OUT IK
         Node(
             package='arm_test_python',
-            executable='arm_test_logic',
-            name='arm_test_logic'
+            executable='ik_subscriber',
+            name='ik_subscriber'
+        ),
+
+        #RUN GRIPPER W/OUT IK
+        # Node(
+        #     package='arm_test_python',
+        #     executable='arm_test_logic',
+        #     name='arm_test_logic'
+        # ),
+
+
+        #Logic for rail
+        Node(
+            package='arm_test_python',
+            executable='rail_subscriber',
+            name='rail_subscriber'
         ),
 
 
