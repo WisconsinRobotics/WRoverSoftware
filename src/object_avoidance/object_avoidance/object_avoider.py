a0a5290ba06bc6fde274cfedea68c48fe93485fc
Aarav Agrawal <2005.aarav.agrawal@gmail.com>
2025-10-01 19:54:55 -0500

    optimized code till gap detection

--- src/object_avoidance/object_avoidance/object_avoider.py
+++ src/object_avoidance/object_avoidance/object_avoider.py
@@ -6,7 +6,7 @@ from cv_bridge import CvBridge
 import numpy as np
 import cv2
 import message_filters
-
+import time
 
 class SectorDepthClassifier(Node):
 
@@ -31,6 +31,7 @@ class SectorDepthClassifier(Node):
         self.pub = self.create_publisher(Image, 'object_avoidance/overlay', 1)
 
     def cb(self, depth_msg: Image):
+        start_time = time.time()
         # Decode and crop depth image
         raw_full = self.bridge.imgmsg_to_cv2(depth_msg, '16UC1')
         
@@ -52,13 +53,16 @@ class SectorDepthClassifier(Node):
         # distance = depth_full[y][x]
         # y is the Uy
         # 
-        for y in range(0,H):
-            for x in range(0, W):
-                height = -1 * depth_full[y][x] * (y - self.Y_PIXEL_OFFSET)/(self.FOCAL_LENGTH)
+        # for y in range(0,H):
+        #     for x in range(0, W):
+        #         height = -1 * depth_full[y][x] * (y - self.Y_PIXEL_OFFSET)/(self.FOCAL_LENGTH)
 
-                if height < -0.5:
-                    depth_full[y][x] = np.float32(10)
+        #         if height < -0.5:
+        #             depth_full[y][x] = np.float32(10)
         
+        rows = (np.arange(depth_full.shape[0], dtype=np.float32) - self.Y_PIXEL_OFFSET) / self.FOCAL_LENGTH
+        mask = -1*depth_full * rows[:, None] < -0.5
+        depth_full[mask] = np.float32(10)
 
         """
         depth_full = (depth_full).astype(np.uint8)
@@ -72,19 +76,24 @@ class SectorDepthClassifier(Node):
             # Publish overlay
         """
         
-        min_list = [] # list of all min values of each vertical sector. values are in m
-        gap_list = [] # list gap tuples
-        for x in range(0, W):
-            min = 1000
-            for y in range(0, H):
-                if depth_full[y][x] < min:
-                    min = depth_full[y][x]
-            if min <= depth_threshold:
-                gap_list.append(1)
-            else:
-                gap_list.append(0)
-            min_list.append(min)
-        print(min_list)
+        # min_list = [] 
+        # gap_list = [] 
+        # for x in range(0, W):
+        #     min = 1000
+        #     for y in range(0, H):
+        #         if depth_full[y][x] < min:
+        #             min = depth_full[y][x]
+        #     if min <= depth_threshold:
+        #         gap_list.append(1)
+        #     else:
+        #         gap_list.append(0)
+        #     min_list.append(min)
+        # print(min_list)
+        
+        # list of all min values of each vertical sector. values are in m
+        min_list = np.min(depth_full, axis = 0)
+        # list of where objects are
+        gap_list = (min_list <= depth_threshold).astype(int)
 
         # if 0 its no object
         # if 1 its no object
@@ -92,35 +101,50 @@ class SectorDepthClassifier(Node):
         # 1 followed by 0 means start of object
         # nothing followed by 0 means start of object
         # 0 followed by nothing means end of object
-        gaps = []
-        gap = ()
-        prev_value = None
-        for index, value in enumerate(gap_list):
+        # gaps = [] # list gap tuples
+        # gap = ()
+        # prev_value = None
+        # for index, value in enumerate(gap_list):
         
-            if index == 0:  # nothing followed by 0 means start of object
-                if value == 0:
-                    gap = gap + (index,)
-                prev_value = value
-                continue
+        #     if index == 0:  # nothing followed by 0 means start of object
+        #         if value == 0:
+        #             gap = gap + (index,)
+        #         prev_value = value
+        #         continue
         
-            if prev_value == 0 and value == 1:  # 0 followed by 1 means end of object
-                gap = gap + (index,)
-                prev_value = value
-                gaps.append(gap)
-                gap = ()
-                continue
+        #     if prev_value == 0 and value == 1:  # 0 followed by 1 means end of object
+        #         gap = gap + (index,)
+        #         prev_value = value
+        #         gaps.append(gap)
+        #         gap = ()
+        #         continue
         
-            if prev_value == 1 and value == 0:  # 1 followed by 0 means start of object
-                gap = gap + (index - 1,)
-                prev_value = value
-                continue
+        #     if prev_value == 1 and value == 0:  # 1 followed by 0 means start of object
+        #         gap = gap + (index - 1,)
+        #         prev_value = value
+        #         continue
         
-            if index == len(gap_list) - 1 and value == 0:  # 0 followed by nothing means end of object
-                gap = gap + (index,)
-                gaps.append(gap)
-                gap = ()
-                continue
+        #     if index == len(gap_list) - 1 and value == 0:  # 0 followed by nothing means end of object
+        #         gap = gap + (index,)
+        #         gaps.append(gap)
+        #         gap = ()
+        #         continue
         
+        d = np.diff(gap_list)
+
+        starts = np.nonzero(d == -1)[0]
+        ends = np.nonzero(d == 1)[0] + 1
+
+        if not gap_list[-1]:
+            ends = np.concatenate((ends, [gap_list.size - 1]))
+        if not gap_list[0]:
+            starts = np.concatenate(([0], starts))
+
+        gaps = list(zip(starts, ends))
+
+
+        # code is optimized till here
+
         thetas = []
         distance_monitor_list = []
         for gap in gaps:
@@ -160,6 +184,8 @@ class SectorDepthClassifier(Node):
         cv2.imshow("obstacle avoidance", depth_full)
         cv2.waitKey(1)
         
+        end_time = time.time() - start_time
+        print(end_time)
         # out_msg = self.bridge.cv2_to_imgmsg(depth_full, 'bgr8')
         # out_msg.header = depth_msg.header
         # self.pub.publish(out_msg)
