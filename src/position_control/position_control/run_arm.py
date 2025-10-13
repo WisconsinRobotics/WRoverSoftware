5390f19badafe0f6519a4e8f2230e25ccc6881d1
ctx-devansh <devansh.the.photofreak@gmail.com>
2025-10-12 22:01:44 -0500

    Final changes, drawing working

--- src/position_control/position_control/run_arm.py
+++ src/position_control/position_control/run_arm.py
@@ -104,9 +104,9 @@ class RunArm(Node):
 
     def process_line_feedback(self):
         """Handle feedback for each liarm_anglesne point."""
-        
+        self.started = True
         if self.point_index < 1 and self.robot_drawing:
-            self.started = True
+            
             self.rail_publisher.publish(self.rail_out_msg)
             self.counter = self.counter + 1
             if self.counter > 20:
@@ -182,9 +182,40 @@ class RunArm(Node):
             # Create Pose
             #self.counter = self.counter - .001
             pose = Pose()
-            pose.position.x = float(-0.9) + self.x / (2600)
+            pose.position.x = float(-1.1) + self.x / (2500)
             pose.position.y = 0.0
-            pose.position.z = float(0.34) - self.y / (2600)
+            pose.position.z = float(0.34) - self.y / (2500)
+
+            self.get_logger().info(f'Processing line: x: {self.x}, y: {self.y}')
+
+
+            # Keep the orientation fixed (90-degree rotation around X-axis)
+            pose.orientation.x = 0.0
+            pose.orientation.y = math.sqrt(.5)# Equivalent to math.pow(2, (1/2)/2)
+            pose.orientation.z = 0.0
+            pose.orientation.w = math.sqrt(.5)  # Equivalent to math.pow(2, (1/2)/2)
+
+            # Create Twist
+            twist = Twist()
+            twist.linear.x = 0.0
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = 0.0
+
+            msg.ee_poses.append(pose)
+            msg.tolerances.append(twist)
+            # Publish messages
+        
+            self.pose_publisher.publish(msg)
+        else:
+            # Create Pose
+            #self.counter = self.counter - .001
+            pose = Pose()
+            pose.position.x = float(-1.0/2)
+            pose.position.y = 0.0
+            pose.position.z = float(0.0) 
 
             self.get_logger().info(f'Processing line: x: {self.x}, y: {self.y}')
 
