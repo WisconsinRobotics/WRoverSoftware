273bc396c5106fedcb27f46bc0579a75a1aa0dae
Balabalu-VE <nicolasdittmarg1@gmail.com>
2025-10-26 00:56:00 -0500

    Flashing oragne

---
+++ src/wr_controller/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'wr_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='old-arm',
+    maintainer_email='nicolasdittmarg1@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'keyboard_controller = wr_controller.keyboard_controller:main'
+        ],
+    },
+)
