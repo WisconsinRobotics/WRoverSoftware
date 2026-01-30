33ed6e3214665f1c916a3a31569448597613d6c8
Nicolas Dittmar Greaves <nicolasdittmarg1@gmail.com>
2026-01-30 09:30:29 -0600

    Cleaned up launch file

--- launch/autonomous_drive_tank.py
+++ launch/autonomous_drive_tank.py
@@ -5,13 +5,8 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='wr_swerve_control',
-            executable='swerve_control',
-            name='swerve_control'
-        ),
-        Node(
-            package='wr_swerve_motor',
-            executable='swerve_motor',
-            name='swerve_motor'
+            executable='tank_control',
+            name='tank_control'
         ),
         Node(
             package='wr_can_comms',
