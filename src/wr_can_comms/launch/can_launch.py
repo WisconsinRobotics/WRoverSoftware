16dc0948678123f959911fb09edcd7de3d60b107
Balabalu-VE <nicolasdittmarg1@gmail.com>
2026-05-05 11:12:26 -0500

    Cleaned up can_comms

---
+++ src/wr_can_comms/launch/can_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        Node(
+            package='wr_can_comms',
+            executable='can_comms',
+            name='can_comms'
+        ),
+
+        Node(
+            package='wr_can_comms',
+            executable='receive_can',
+            name='receive_can'
+        ),
+
+
+    ])
+    
--- src/wr_can_comms/setup.py
+++ src/wr_can_comms/setup.py
@@ -1,15 +1,18 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'wr_can_comms'
 
 setup(
-    name=package_name,
+     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
     ],
     install_requires=['setuptools', 'python-can'],
     zip_safe=True,
